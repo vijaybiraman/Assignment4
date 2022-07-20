a4ed5c9e9e5f64b96457c8aa03a7c62c8e1ab171
vijaybiraman <109381804+vijaybiraman@users.noreply.github.com>
2022-07-20 07:39:59 +0530

    Add files via upload

---
+++ CheckStringBuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demowork1
+{
+    internal class CheckStringBuilder
+    {
+        static void Main(string[] args)
+        {
+            string str = "hello";
+            str = str + "world";
+            Console.WriteLine(str);
+            StringBuilder sb = new StringBuilder("hello");
+            sb.Append("java");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SwapNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapNumber
+{
+    internal class SwapNumber
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+            SwapNumbers(ref x, ref y);
+            Console.WriteLine("x : " + x);
+            Console.WriteLine("y : " + y);
+
+            Console.ReadLine();
+        }
+
+        static void SwapNumbers(ref int x,ref int y)
+        {
+           int z = x + y;
+            x = z - x;
+            y = z - y;
+        }
+    }
+
+}
